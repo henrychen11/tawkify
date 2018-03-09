c9a120bebab056371398c52c73e97e760f55cb72
Henry Chen <henrychen1@gmail.com>
2018-03-08 20:03:30 -0800

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,9 @@ var path = require('path');
 
 module.exports = {
     devtool: 'inline-source-map',
-    entry: [
-        'webpack-dev-server/client?http://127.0.0.1:8080/',
-        'webpack/hot/only-dev-server',
-        './src'
-    ],
+    entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname),
         filename: 'bundle.js'
     },
     resolve: {
